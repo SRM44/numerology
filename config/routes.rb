5653a4ad248a3bd8347ffac68c9d8f30daeab28b
Steven Rigolet <steven-r@hotmail.fr>
2020-01-14 16:53:26 +0100

    rails new with frontend and form gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  get 'contact', to: 'pages#contact', as: :contact
-  get 'theme', to: 'pages#theme', as: :theme
+  get 'clients', to: 'clients#index'
+
+  get 'clients/new', to: 'clients#new'
+  post 'clients', to: 'clients#create'
+
+  get 'clients/:id', to: 'clients#show', as: :client
+
+  get 'clients/:id/edit', to: 'clients#edit'
+  patch 'clients/:id', to: 'clients#update'
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
