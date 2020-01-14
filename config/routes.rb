437002ab2b3b881df823e7841819ca4e9b6b1212
Steven Rigolet <steven-r@hotmail.fr>
2020-01-14 15:15:41 +0100

    add client model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'theme', to: 'pages#theme', as: :theme
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
