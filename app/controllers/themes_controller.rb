0738764c85bf8a8a0b6bb22704e1300aa4ef103f
Steven Rigolet <stevemoriconi44@gmail.com>
2020-04-16 19:19:27 +0200

    Need to find a way to puts model datas from life map into the view.

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -8,8 +8,7 @@ class ThemesController < ApplicationController
   end
 
   def show
-    letters_to_numbers
-    count_numbers
+
     # life_map
   end
 
@@ -38,91 +37,6 @@ class ThemesController < ApplicationController
 
     redirect_to themes_path(@theme)
   end
-
-
-
-
-
-def age
-  now = Time.current
-  @dob = Date.new(@theme.birth_year,@theme.birth_month,@theme.birth_day)
-  now.year - @dob.year - ((now.month > @dob.month || (now.month == @dob.month && now.day >= @dob.day)) ? 0 : 1)
-end
-
-# TO DO
-# AGE
-# Zodiac sign
-# SPIRAL OF THE DAY
-
-# EXPRESSION FIELD CALCULATION
-def expression_field
-  cerebral =  ['a', 'h', 'j', 'n', 'p', 'g', 'l']
-  physics =   ['e', 'w', 'd', 'm']
-  emotive =   ['i', 'r', 'z', 'o', 'b', 's', 't', 'x']
-  intuitive = ['k', 'q', 'u', 'y', 'c', 'v']
-
-  # pour chaque lettre du tableau fullname
-  # compter le nombre de lettre present dans les tableaux de reference
-  #
-end
-
-
-#SPIRAL CALCULATION
-
-    #1 red
-    #2 bleu
-    #3 jaune
-    #4 marron
-    #5 vert clair
-    #6 rose
-    #7 violet
-    #8 vert foncé
-    #9 safran / blanc
-
-  #EXPERIENCE FIELD CALCULATIONS
-
-  def fullname
-    @fullname = (@theme.name.strip + @theme.name1.strip + @theme.name2.strip + @theme.name3.strip + @theme.surname.strip + @theme.surname1.strip + @theme.surname2.strip + @theme.surname3.strip).downcase.chars
-  end
-
-  def letters_to_numbers
-    fullname
-    @reference = {}
-    letters = ('a'..'z').to_a
-    letters.each_with_index do |letter, index|
-      @reference[letter] = (index) % 9 + 1
-    end
-    @results = []
-    @fullname.each do |letter|
-      @results << @reference[letter]
-    end
-  end
-
-
-  def count_numbers
-    @counts = {
-      1 => 0,
-      2 => 0,
-      3 => 0,
-      4 => 0,
-      5 => 0,
-      6 => 0,
-      7 => 0,
-      8 => 0,
-      9 => 0
-    }
-    @results.each do |result|
-      @counts[result] += 1
-    end
-  end
-
-  # LIFE MAP CALCULATION
-  # def life_map
-  #   @action1 = (@physics + @emotional).to_s.chars.map(&:to_i).sum
-  #   @action2 = (@physics + @creative).to_s.chars.map(&:to_i).sum
-  #   @action3 = (@action1 + @action2).to_s.chars.map(&:to_i).sum
-  #   @action4 = (@emotional + @creative).to_s.chars.map(&:to_i).sum
-  # end
 
   private
 
--- app/models/theme.rb
+++ app/models/theme.rb
@@ -4,9 +4,56 @@ class Theme < ApplicationRecord
     self.name.strip.capitalize + " " + self.surname.strip.capitalize
   end
 
- def detailled_name
+  def detailled_name
     self.name.strip.capitalize + " " + self.name1.strip.capitalize  + " " + self.name2.strip.capitalize   + " " + self.name3.strip.capitalize + " " + self.surname.strip.capitalize + " " + self.surname1.strip.capitalize  + " " + self.surname2.strip.capitalize   + " " + self.surname3.strip.capitalize
   end
+
+#EXPERIENCE FIELD CALCULATIONS
+  def experience_field
+    (self.name.strip + self.name1.strip + self.name2.strip + self.name3.strip + self.surname.strip + self.surname1.strip + self.surname2.strip + self.surname3.strip).downcase.chars
+  end
+
+  def letters_to_numbers
+    @reference = {}
+    letters = ('a'..'z').to_a
+    letters.each_with_index do |letter, index|
+      @reference[letter] = (index) % 9 + 1
+    end
+    @results = []
+    @experience_field.each do |letter|
+      @results << @reference[letter]
+    end
+  end
+
+
+  def count_numbers
+    @counts = {
+      1 => 0,
+      2 => 0,
+      3 => 0,
+      4 => 0,
+      5 => 0,
+      6 => 0,
+      7 => 0,
+      8 => 0,
+      9 => 0
+    }
+    @results.each do |result|
+      @counts[result] += 1
+    end
+  end
+
+#SPIRAL CALCULATION
+
+    #1 red
+    #2 bleu
+    #3 jaune
+    #4 marron
+    #5 vert clair
+    #6 rose
+    #7 violet
+    #8 vert foncé
+    #9 safran / blanc
 
   def power(number)
     if [11, 22, 33].include? number
@@ -46,4 +93,38 @@ class Theme < ApplicationRecord
     power(number)
   end
 
+# LIFE MAP CALCULATION
+  def action_one
+    (self.birth_day + self.birth_month).to_s.chars.map(&:to_i).sum.to_s.chars.map(&:to_i).sum
+  end
+
+  def action_two
+    (self.birth_day + self.birth_year).to_s.chars.map(&:to_i).sum.to_s.chars.map(&:to_i).sum
+  end
+
+  def action_three
+    (action_one + action_two).to_s.chars.map(&:to_i).sum.to_s.chars.map(&:to_i).sum
+  end
+
+  def action_four
+    (self.birth_month + self.birth_year).to_s.chars.map(&:to_i).sum.to_s.chars.map(&:to_i).sum
+  end
+
+
+  # TO DO
+  # AGE
+  # Zodiac sign
+  # SPIRAL OF THE DAY
+
+  # EXPRESSION FIELD CALCULATION
+  def expression_field
+    cerebral =  ['a', 'h', 'j', 'n', 'p', 'g', 'l']
+    physics =   ['e', 'w', 'd', 'm']
+    emotive =   ['i', 'r', 'z', 'o', 'b', 's', 't', 'x']
+    intuitive = ['k', 'q', 'u', 'y', 'c', 'v']
+
+    # pour chaque lettre du tableau fullname
+    # compter le nombre de lettre present dans les tableaux de reference
+    #
+  end
 end
